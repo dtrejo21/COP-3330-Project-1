38f55b50e84268bb008c2408e63500add9a62c06
dtrejo21 <54925389+dtrejo21@users.noreply.github.com>
2022-10-26 16:04:41 -0400

    Add files via upload

--- main.java
+++ main.java
@@ -1,6 +1,16 @@
-//Project 1
 //Daisy Trejo-Hernandez
 // 
 // 
 
+import java.util.Scanner;
 
+public class main
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
